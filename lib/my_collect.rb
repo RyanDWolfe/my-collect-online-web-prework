bb0abeb0757003f3fd376d6d637222a54a40df20
Ryan Wolfe <ryan@studiohopfitness.com>
2019-01-29 22:23:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 def my_collect(array)
   i = 0
-  result = while i < array.length
+  while i < array.length
     yield array.join(", ")
 
     i += 1
   end
-  result
+  
 end
 
 # array = array.join(", ")
